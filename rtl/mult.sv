179d271de27dfb7372d14f455b53593bed58e63b
Sheng-Hao <shenghah@andrew.cmu.edu>
2019-03-26 16:45:15 -0400

    Updated multiplier internals for 32 bit output

--- rtl/mult.sv
+++ rtl/mult.sv
@@ -4,29 +4,30 @@ module multiplier(
   input  logic [31:0]  a, b,
   output logic [31:0]  out);
 
-  logic [31:0][63:0] partial_products;
-  logic [3:0][63:0] intermed_sums;
-  logic [31:0][31:0] b_bits;
-  logic [63:0] sum;
+  logic [31:0][31:0] partial_products;
+  logic [3:0][31:0] intermed_sums;
+  logic [31:0] sum;
 
+
+  /*
+     10110
+      1001
+     -----
+     10110
+    00000
+   00000
+  10110
+  --------
+  11000110
+  */
   genvar i, j, k;
   generate
     for (i = 0; i < 32; i=i+1) begin: find_partial_products
       for (j = 0; j < i; j=j+1) begin: right_zeros
         assign partial_products[i][j] = 1'b0;
       end
-      assign partial_products[i][i+32:i] = a & b_bits[i];
-      for (k = i+32; k < 64; k=k+1) begin: left_zeros
-        assign partial_products[i][k] = 1'b0;
-      end
-    end
-  endgenerate
-
-  genvar l, m;
-  generate
-    for (l = 0; l < 32; l=l+1) begin: loop_through_b_bits
-      for (m = 0; m < 32; m=m+1) begin: extend_32
-        b_bits[l][m] = b[l]
+      for (k = i; k < 32; k=k+1) begin: remaining
+        assign partial_products[i][k-i] = a[k-i] & b[i];
       end
     end
   endgenerate
@@ -50,6 +51,6 @@ module multiplier(
   assign sum = intermed_sums[0] + intermed_sums[1] + intermed_sums[2] + 
                intermed_sums[3];
 
-  assign out = sum[31:0]
+  assign out = sum;
   
 endmodule
