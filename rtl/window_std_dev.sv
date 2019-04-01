7366974c8543f6f3a131408f465e0f39d17c38d8
Sheng-Hao Huang <shenghah@andrew.cmu.edu>
2019-04-01 01:48:49 -0400

    Optimized away a multiplier in window_std_dev

--- rtl/window_std_dev.sv
+++ rtl/window_std_dev.sv
@@ -21,7 +21,7 @@ module window_std_dev(
   assign scan_win_sq_sum = scan_win_bottom_right_sq - scan_win_bottom_left_sq + scan_win_top_left_sq - scan_win_top_right_sq;
   assign scan_win_sum = scan_win_bottom_right - scan_win_bottom_left + scan_win_top_left - scan_win_top_right;
 
-  multiplier scan_win_mult1(.out(scan_win_std_dev1), .a(scan_win_sq_sum), .b(32'd576));
+  assign scan_win_std_dev1 = {scan_win_sq_sum[22:0], 9'd0} + {scan_win_sq_sum[25:0], 6'd0};
   multiplier scan_win_mult2(.out(scan_win_std_dev2), .a(scan_win_sum), .b(scan_win_sum));
 
   sqrt stddev(.val(scan_win_std_dev1 - scan_win_std_dev2), .res(scan_win_std_dev));
