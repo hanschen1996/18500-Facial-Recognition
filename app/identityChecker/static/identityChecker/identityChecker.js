dad8d7cc7a07988dba57c699b8d4d3b4fb2a9ab3
Andy <andyshen2882@gmail.com>
2019-04-01 00:52:42 -0400

    Improved formatting of 5 images taken

--- app/identityChecker/static/identityChecker/identityChecker.js
+++ app/identityChecker/static/identityChecker/identityChecker.js
@@ -12,35 +12,35 @@ function captureImages() {
 
       timeleft -= 1;
       if(timeleft == 19){
-        context.drawImage(player, 0, 0, canvas.width, canvas.height);
+        context.drawImage(player, 0, 0, 160, 120);
       }
         
     if(timeleft == 14) {
         const canvas2 = document.getElementById('canvas2');
         const context2 = canvas2.getContext('2d');
         
-        context2.drawImage(player, 0, 0, canvas2.width, canvas2.height);
+        context2.drawImage(player, 0, 0, 160, 120);
     }
     
     if(timeleft == 9) {
         const canvas3 = document.getElementById('canvas3');
         const context3 = canvas3.getContext('2d');
         
-        context3.drawImage(player, 0, 0, canvas3.width, canvas3.height);
+        context3.drawImage(player, 0, 0, 160, 120);
     }
         
     if(timeleft == 4) {
         const canvas4 = document.getElementById('canvas4');
         const context4 = canvas4.getContext('2d');
         
-        context4.drawImage(player, 0, 0, canvas4.width, canvas4.height);
+        context4.drawImage(player, 0, 0, 160, 120);
     }
         
     if(timeleft < 0) {
         const canvas5 = document.getElementById('canvas5');
         const context5 = canvas5.getContext('2d');
         
-        context5.drawImage(player, 0, 0, canvas5.width, canvas5.height);
+        context5.drawImage(player, 0, 0, 160, 120);
         document.getElementById("countdown").innerHTML = "Finished"
         clearInterval(downloadTimer);
 
