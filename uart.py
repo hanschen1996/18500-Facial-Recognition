080b4432d8ee13817a5845d0b848923423f168d8
Sheng-Hao <shenghah@andrew.cmu.edu>
2019-04-03 08:32:16 -0400

    Updated uart code for sending image

--- uart.py
+++ uart.py
@@ -2,22 +2,27 @@ import serial
 import sys
 from PIL import Image
 
-img = Image.open('image.png').convert('LA')
-img.save('greyscale.png')
-
 # request to send = rts
 # clear to send = cts
 # DONT FORGET TO SEND TWO DUMMY BYTES AFTER LAPTOP IMAGE
 
-with open("greyscale.png", "rb") as image:
-  f = image.read()
-  b = bytearray(f)
+img = Image.open('image.png').convert('L')
+WIDTH, HEIGHT = img.size
+data = list(img.getdata())
+data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
 
-ser = serial.Serial(port='COM3', baudrate=921600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+ser = serial.Serial(port='COM3', baudrate=921600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=None)
 ser.close()
 ser.open()
-ser.write(b'hi')
-print("wrote hi")
-print("recieved: " + str(ser.read(10000)))
+
+for i in range(HEIGHT):
+    for j in range(WIDTH):
+        ser.write(b"%0.2X" % data[i][j])
+ser.write(b"FF")
+ser.write(b"FF")
+
+while (True):
+    ser.read()
+    wait(10)
+
 ser.close()
-# ser.getSupportedBaudRates()
