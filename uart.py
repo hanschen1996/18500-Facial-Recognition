a24dfdb075298407c320372e1e15864ab11caf36
Sheng-Hao <shenghah@andrew.cmu.edu>
2019-04-03 12:09:24 -0400

    Finished uart python script for now

--- uart.py
+++ uart.py
@@ -1,6 +1,7 @@
 import serial
 import sys
 from PIL import Image
+import time
 
 # request to send = rts
 # clear to send = cts
@@ -11,18 +12,15 @@ WIDTH, HEIGHT = img.size
 data = list(img.getdata())
 data = [data[offset:offset+WIDTH] for offset in range(0, WIDTH*HEIGHT, WIDTH)]
 
-ser = serial.Serial(port='COM3', baudrate=921600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=None)
+ser = serial.Serial(port='COM3', baudrate=921600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, timeout=2)
 ser.close()
 ser.open()
 
 for i in range(HEIGHT):
     for j in range(WIDTH):
-        ser.write(b"%0.2X" % data[i][j])
-ser.write(b"FF")
-ser.write(b"FF")
+        ser.write(bytes([data[i][j]]))
 
-while (True):
-    ser.read()
-    wait(10)
+ser.read()
+time.sleep(10)
 
 ser.close()
