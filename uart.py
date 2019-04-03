3a01476a7d4d4a5b46238ba0127d46825e529182
Sheng-Hao Huang <shenghah@andrew.cmu.edu>
2019-04-03 08:03:33 -0400

    added top_tb

--- uart.py
+++ uart.py
@@ -2,22 +2,22 @@ import serial
 import sys
 from PIL import Image
 
-img = Image.open('image.png').convert('LA')
-img.save('greyscale.png')
-
 # request to send = rts
 # clear to send = cts
 # DONT FORGET TO SEND TWO DUMMY BYTES AFTER LAPTOP IMAGE
 
-with open("greyscale.png", "rb") as image:
-  f = image.read()
-  b = bytearray(f)
+img = Image.open('image.png').convert('LA')
+img.save('greyscale.png')
 
-ser = serial.Serial(port='COM3', baudrate=921600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
-ser.close()
-ser.open()
-ser.write(b'hi')
-print("wrote hi")
-print("recieved: " + str(ser.read(10000)))
-ser.close()
+img = open("greyscale.png", "rb").read()
+b = bytearray(img)
+print(len(b))
+
+# ser = serial.Serial(port='COM3', baudrate=921600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+# ser.close()
+# ser.open()
+# ser.write(b'hi')
+# print("wrote hi")
+# print("recieved: " + str(ser.read(10000)))
+# ser.close()
 # ser.getSupportedBaudRates()
