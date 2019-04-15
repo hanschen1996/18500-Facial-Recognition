c5a29f89288f53340b713f1f3a78c84cfd70306e
Andy <andyshen2882@gmail.com>
2019-04-15 01:12:02 -0400

    Get images to automatically download when taken

--- app/identityChecker/static/identityChecker/identityChecker.js
+++ app/identityChecker/static/identityChecker/identityChecker.js
@@ -1,39 +1,78 @@
+var download = function(){
+  var link = document.createElement('a');
+  link.download = 'image.png';
+  link.href = document.getElementById('canvas1').toDataURL()
+  link.click();
+}
+
+var download2 = function(){
+  var link = document.createElement('a');
+  link.download = 'image2.png';
+  link.href = document.getElementById('canvas2').toDataURL()
+  link.click();
+}
+
+var download3 = function(){
+  var link = document.createElement('a');
+  link.download = 'image3.png';
+  link.href = document.getElementById('canvas3').toDataURL()
+  link.click();
+}
+
+var download4 = function(){
+  var link = document.createElement('a');
+  link.download = 'image4.png';
+  link.href = document.getElementById('canvas4').toDataURL()
+  link.click();
+}
+
+var download5 = function(){
+  var link = document.createElement('a');
+  link.download = 'image5.png';
+  link.href = document.getElementById('canvas5').toDataURL()
+  link.click();
+}
+
 function captureImages() {
     var count = 0;
 
-    var timeleft = 25;
+    var timeleft = 10;
     var downloadTimer = setInterval(function(){
-    if(timeleft % 5 == 0) {
-        document.getElementById("countdown").innerHTML = 5 + " Seconds Until Photo is Taken";
+    if(timeleft % 2 == 0) {
+        document.getElementById("countdown").innerHTML = 2 + " Seconds Until Photo is Taken";
     } 
     else {
-        document.getElementById("countdown").innerHTML = timeleft % 5 + " Seconds Until Photo is Taken";
+        document.getElementById("countdown").innerHTML = timeleft % 2 + " Seconds Until Photo is Taken";
     }
 
       timeleft -= 1;
-      if(timeleft == 19){
+      if(timeleft == 7){
         context.drawImage(player, 0, 0, 160, 120);
+        download();
       }
         
-    if(timeleft == 14) {
+    if(timeleft == 5) {
         const canvas2 = document.getElementById('canvas2');
         const context2 = canvas2.getContext('2d');
         
         context2.drawImage(player, 0, 0, 160, 120);
+        download2();
     }
     
-    if(timeleft == 9) {
+    if(timeleft == 3) {
         const canvas3 = document.getElementById('canvas3');
         const context3 = canvas3.getContext('2d');
         
         context3.drawImage(player, 0, 0, 160, 120);
+        download3();
     }
         
-    if(timeleft == 4) {
+    if(timeleft == 1) {
         const canvas4 = document.getElementById('canvas4');
         const context4 = canvas4.getContext('2d');
         
         context4.drawImage(player, 0, 0, 160, 120);
+        download4();
     }
         
     if(timeleft < 0) {
@@ -41,7 +80,9 @@ function captureImages() {
         const context5 = canvas5.getContext('2d');
         
         context5.drawImage(player, 0, 0, 160, 120);
+        download5();
         document.getElementById("countdown").innerHTML = "Finished"
+        
         clearInterval(downloadTimer);
 
     }
@@ -49,12 +90,4 @@ function captureImages() {
     console.log("count: " + count);
     console.log("timeleft: " + timeleft);
     }, 1000);
-}
-
-function download(){    
-    var download = document.getElementById("download");
-    var image = canvas.toDataURL("image/pgm").replace("image/png", "image/octet-stream");
-
-    download.setAttribute("href", image);
-    
 }
